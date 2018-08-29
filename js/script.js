e04c8631e9b7faf385cf048b0adff1b74e1e073d
Adam Basiak <basiakus@gmail.com>
2018-08-29 22:23:23 +0200

    push

---
+++ js/script.js
@@ -0,0 +1,18 @@
+	const speechSpeed = document.querySelector('.speed');
+	const speedBar = speechSpeed.querySelector('.speed-bar');
+	const video = document.querySelector('.flex');
+
+	function sppedOfSpeech(e) {
+		const valueOFSpeedBar = e.pageY - this.offsetTop;
+		const proc = valueOFSpeedBar / this.offsetHeight;
+		const slowest = 0.5;
+		const fastest = 5;
+		const height = Math.round(proc * 100) + '%';
+		const playbackRate = proc * (fastest - slowest) + slowest;
+		speedBar.style.height = height;
+		speedBar.textContent = playbackRate.toFixed(2) + '×';
+		video.playbackRate = playbackRate;
+		console.log(proc);
+	}
+
+	speechSpeed.addEventListener('mousemove', sppedOfSpeech);
